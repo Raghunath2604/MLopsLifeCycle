08ac8e27cd868a82ac488882fed499106a3e9ae5
RAGHUNATHAREDDY GR <raghunathareddygr94@gmail.com>
2026-06-16 23:47:13 +0530

    Fix MLflow query to ignore FAILED runs missing S3 artifacts

--- prediction_model/predict.py
+++ prediction_model/predict.py
@@ -38,7 +38,7 @@ def generate_predictions(data_input):
     experiment_name = config.EXPERIMENT_NAME
     experiment = mlflow.get_experiment_by_name(experiment_name)
     experiment_id = experiment.experiment_id
-    runs_df=mlflow.search_runs(experiment_ids=experiment_id,order_by=['metrics.f1_score DESC'])
+    runs_df=mlflow.search_runs(experiment_ids=experiment_id, filter_string="status = 'FINISHED'", order_by=['metrics.f1_score DESC'])
     best_run=runs_df.iloc[0]
     best_run_id=best_run['run_id']
     best_model='runs:/' + best_run_id + config.MODEL_NAME
@@ -64,7 +64,7 @@ def generate_predictions_batch(data_input):
     experiment_name = config.EXPERIMENT_NAME
     experiment = mlflow.get_experiment_by_name(experiment_name)
     experiment_id = experiment.experiment_id
-    runs_df=mlflow.search_runs(experiment_ids=experiment_id,order_by=['metrics.f1_score DESC'])
+    runs_df=mlflow.search_runs(experiment_ids=experiment_id, filter_string="status = 'FINISHED'", order_by=['metrics.f1_score DESC'])
     best_run=runs_df.iloc[0]
     best_run_id=best_run['run_id']
     best_model='runs:/' + best_run_id + config.MODEL_NAME
